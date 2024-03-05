4d2cedc0b3e955a06e830ea99ddd36e293d741a9
Brendon <brendonp@excelerate.info>
2024-03-05 10:50:10 +0000

    V2, Updated text display to be more readable and fixed original timing problems. Arduino can be left plugged in and will start battery test when voltage detected, automatically stops testing when below minimum voltage dishcharge, now only checks every 5 seconds instead of every 1.

--- BatteryTesterScript.ino
+++ BatteryTesterScript.ino
@@ -13,30 +13,26 @@ Use putty serial, set up logging functionality, saves all serial output to txt f
 TODO: Remove full date and just do time elapsed - clock_t
 */
 #include <stdio.h>
-#include <time.h>
+#include "Timer.h"
 
+Timer         timer;
 float         value;
 float         voltage;
 float         convertedValueDisplay;
 float         convertedBatteryVoltage;
 float         firstRead;
 float         lastRead;
+float         checkValue;
+float         checkConvValue;
+float         checkBattVolt;
 bool          hasCharge;
-// static time_t myTime;
-uint8_t       displayTimeHour;
-uint8_t       displayTimeMinute;
-uint8_t       displayTimeSecond;
+bool          startUp                 = false;
 const float   finalVoltageAtDischarge = 8.25;        // Look for battery data sheet and find final voltage
-const float   Vin                     = 12.58;       // Change as needed
-const float   Vout                    = 2.51;        // Change as needed07
+const float   Vin                     = 12.58;       // Check values for each battery used, use multimeter and input values here.
+const float   Vout                    = 2.51;        // Check values for each battery used, use multimeter and input values here.
+const float   batteryConversionValue  = Vin / Vout;
 const float   conversionValue         = 3.3 / 1024;  // Maximum voltage of microcontroller / ADC resolution (inputRead funciton has 1024 resolution max)
 const uint8_t analogPin               = A2;          // Input pin of microcontroller
-const float   batteryConversionValue  = Vin / Vout;
-bool          startUp                 = false;
-
-float checkValue;
-float checkConvValue;
-float checkBattVolt;
 
 void checkSerial() {
   checkValue = analogRead(analogPin);
@@ -44,32 +40,23 @@ void checkSerial() {
   checkBattVolt = checkConvValue * batteryConversionValue;
 
   (checkBattVolt > finalVoltageAtDischarge) ? hasCharge = true : hasCharge = false;
-  // if (hasCharge) {
-  //   myTime = time(0);
-  // }
-  Serial.println("CheckSerial Reached");
+  Serial.println("Checking for power input...");
 }
 
 void checkBattery(){
-  // Reset timer for battery function
-  static time_t myTime;
-  myTime            = time(NULL);
-  displayTimeHour   = 0;
-  displayTimeMinute = 0;
-  displayTimeSecond = 0;
-
+  (hasCharge) ? timer.start() : timer.stop();
   while (hasCharge) {
+    uint8_t totalSeconds = (timer.read() / 1000) % 60;
+    uint8_t totalMinutes = ((timer.read() / 1000) / 60) % 60;
+    uint8_t totalHours   = ((timer.read() / 1000) / 60) / 60;
+    Serial.printf("TIME:\t\t\t   %02d:%02d:%02d\n", totalHours, totalMinutes, totalSeconds);
+
     value = analogRead(analogPin);
-    myTime = time(NULL);
-    displayTimeHour   = (myTime / 3600) % 24;
-    displayTimeMinute = (myTime / 60) % 60;
-    displayTimeSecond = myTime % 60;
-    Serial.printf("Time: %02d:%02d:%02d\n", displayTimeHour, displayTimeMinute, displayTimeSecond);
-    Serial.print("First reading value: ");
+    Serial.print("First reading value:       ");
     Serial.println(value);
 
     convertedValueDisplay = value * conversionValue;
-    Serial.print("Converted value: ");
+    Serial.print("Converted value:           ");
     Serial.println(convertedValueDisplay);
 
     convertedBatteryVoltage = convertedValueDisplay * batteryConversionValue;  // Need to see if consistent across all batteries
@@ -80,9 +67,11 @@ void checkBattery(){
     (startUp) ? firstRead = convertedBatteryVoltage : startUp = false;
     (convertedBatteryVoltage <= finalVoltageAtDischarge) ? hasCharge = false : lastRead = convertedBatteryVoltage;
     startUp = false;
-    delay(1000);
+    delay(5000);
   }
-  Serial.println("-------------Battery dead-------------");
+
+  Serial.printf("Battery test is now finished. \nFirst read: %f, Final read: %f", firstRead, lastRead);
+  Serial.println("\n-------------Battery dead-------------");
 }
 
 void setup() {
@@ -92,6 +81,7 @@ void setup() {
 }
 
 void loop() {
+  delay(4000);
   checkSerial();
   checkBattery();
 }
